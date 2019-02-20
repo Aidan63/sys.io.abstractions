067b65675c4a8eba1de577d2df33555892a39501
Aidan <aidan.lee63@gmail.com>
2019-02-20 20:44:39 +0000

    Moved ci script into the ci config file

--- ci.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm install git+https://git@github.com/starburst997/lix.client.git --global
-
-lix download
-
-haxe test.hxml -D reporter=test.XUnit2Reporter -D report-name=Test-Results-$AGENT_OS
